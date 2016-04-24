954c3d682c9089e45a0d261aa59284e2c5b4965f
Sandro Wirth <s.wirth303@gmail.com>
2016-04-24 18:21:38 +0200

    fixed house rotation

--- source/src/house3d/House3D.java
+++ source/src/house3d/House3D.java
@@ -2,51 +2,86 @@ package house3d;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class House3D extends JPanel{
 
     private List<Point3D> points;
     private double ds;
-    private double dO;
+    private double d0;
+    private int offset;
+    private int scale;
 
-    public House3D() {
+    public House3D(double ds, double d0, int offset, int scale) {
         points = new ArrayList<Point3D>();
 
-        ds = 9;
-        dO = 10;
+        this.ds = ds;
+        this.d0 = d0;
+        this.offset = offset;
+        this.scale = scale;
 
-        //Start Points
-        points.add(new Point3D(0.5, 0, 1.5));
-        points.add(new Point3D(0, 0, 1));
+        //Front
         points.add(new Point3D(0, 0, 0));
         points.add(new Point3D(1, 0, 0));
-        points.add(new Point3D(1, 0, 1));
-        points.add(new Point3D(0.5, 1, 1.5));
-        points.add(new Point3D(0, 1, 1));
-        points.add(new Point3D(0, 1, 0));
         points.add(new Point3D(1, 1, 0));
+        points.add(new Point3D(0, 1, 0));
+        points.add(new Point3D(0.5, 1.5, 0));
+
+        //Back
+        points.add(new Point3D(0, 0, 1));
+        points.add(new Point3D(1, 0, 1));
         points.add(new Point3D(1, 1, 1));
+        points.add(new Point3D(0, 1, 1));
+        points.add(new Point3D(0.5, 1.5, 1));
+    }
+
+    public void rotateZ(double angle) {
+        for (Point3D point : points) {
+            point.rotateZ(angle);
+        }
     }
 
     public void rotateX(double angle) {
+        for (Point3D point : points) {
+            point.rotateX(angle);
+        }
+    }
+
+    public void rotateY(double angle) {
         for (Point3D point : points) {
             point.rotateY(angle);
         }
     }
 
 
+
     private void doDrawing(Graphics g, List<Point3D> points) {
 
         Graphics2D g2d = (Graphics2D) g;
 
-        //Paints all lines of the house
-        for (int i = 0; i < 4; i++) {
-            drawLine(points.get(i), points.get(i + 1), g2d);
-            drawLine(points.get(i + 5), points.get(i + 6), g2d);
-            drawLine(points.get(i), points.get(i + 5), g2d);
-        }
+        //Front
+        drawLine(points.get(0), points.get(1), g2d, Color.RED);
+        drawLine(points.get(1), points.get(2), g2d, Color.RED);
+        drawLine(points.get(2), points.get(3), g2d, Color.RED);
+        drawLine(points.get(0), points.get(3), g2d, Color.RED);
+        drawLine(points.get(2), points.get(4), g2d, Color.RED);
+        drawLine(points.get(3), points.get(4), g2d, Color.RED);
+
+        //Back
+        drawLine(points.get(5), points.get(6), g2d, Color.BLUE);
+        drawLine(points.get(6), points.get(7), g2d, Color.BLUE);
+        drawLine(points.get(7), points.get(8), g2d, Color.BLUE);
+        drawLine(points.get(5), points.get(8), g2d, Color.BLUE);
+        drawLine(points.get(7), points.get(9), g2d, Color.BLUE);
+        drawLine(points.get(8), points.get(9), g2d, Color.BLUE);
+
+        //Side and Top
+        drawLine(points.get(4), points.get(9), g2d, Color.GREEN);
+        drawLine(points.get(3), points.get(8), g2d, Color.GREEN);
+        drawLine(points.get(2), points.get(7), g2d, Color.GREEN);
+        drawLine(points.get(1), points.get(6), g2d, Color.GREEN);
+        drawLine(points.get(0), points.get(5), g2d, Color.GREEN);
     }
 
     @Override
@@ -56,16 +91,16 @@ public class House3D extends JPanel{
     }
 
 
-    private void drawLine(Point3D point1, Point3D point2, Graphics2D g) {
-        int x1 = 300 + 400 * (int) Math.round((this.ds / (this.dO + point1.y)) * point1.x);
-        int y1 = 300 + 400 * (int) Math.round((this.ds / (this.dO + point1.y)) * point1.z);
+    private void drawLine(Point3D point1, Point3D point2, Graphics2D g, Color color) {
+        double x1 = this.offset + this.scale * (this.ds / (this.d0 + point1.z)) * point1.x;
+        double y1 = this.offset + this.scale * (this.ds / (this.d0 + point1.z)) * point1.y;
 
-        int x2 =  300 + 400 * (int) Math.round((this.ds / (this.dO + point2.y)) * point2.x);
-        int y2 =  300 + 400 * (int) Math.round((this.ds / (this.dO + point2.y)) * point2.z);
+        double x2 = this.offset + this.scale *(this.ds / (this.d0 + point2.z)) * point2.x;
+        double y2 = this.offset + this.scale *(this.ds / (this.d0 + point2.z)) * point2.y;
 
-        g.drawLine(x1, y1, x2, y2);
+        g.setColor(color);
+        g.drawLine((int) x1, (int) y1, (int)x2, (int) y2);
     }
-
 
 
     class Point3D {
@@ -77,9 +112,34 @@ public class House3D extends JPanel{
             this.z = z;
         }
 
+        public void rotateZ(double angle) {
+            angle = Math.toRadians(angle);
+
+            double tempX = this.x;
+            double tempY = this.y;
+
+            this.x = Math.cos(angle) * tempX+ Math.sin(angle) * tempY;
+            this.y = -Math.sin(angle) * tempX + Math.cos(angle) * tempY;
+        }
+
+        public void rotateX(double angle) {
+            angle = Math.toRadians(angle);
+
+            double tempY = this.y;
+            double tempZ = this.z;
+
+            this.y = Math.cos(angle) * tempY + Math.sin(angle) * tempZ;
+            this.z = -Math.sin(angle) * tempY + Math.cos(angle) * tempZ;
+        }
+
         public void rotateY(double angle) {
-            this.x = Math.cos(angle) * this.x + Math.sin(this.x);
-            this.z = -Math.sin(angle) * this.z + Math.cos(angle) * this.z;
+            angle = Math.toRadians(angle);
+
+            double tempX = this.x;
+            double tempZ = this.z;
+
+            this.x = Math.cos(angle) * tempX + Math.sin(angle) * tempZ;
+            this.z = -Math.sin(angle) * tempX + Math.cos(angle) * tempZ;
         }
     }
 
--- source/src/house3d/House3DExample.java
+++ source/src/house3d/House3DExample.java
@@ -2,47 +2,153 @@ package house3d;
 
 import javax.swing.*;
 import java.awt.*;
+import java.text.Format;
+import java.text.NumberFormat;
 
-public class House3DExample extends JFrame {
+public class House3DExample extends JFrame  {
 
     private House3D house;
+    private JButton updateButton;
+    private JCheckBox xCheckbox;
+    private JCheckBox yCheckbox;
+    private JCheckBox zCheckbox;
+
+    private JFormattedTextField xAngleTextField;
+    private JFormattedTextField yAngleTextField;
+    private JFormattedTextField zAngleTextField;
+
+    private JFormattedTextField msTextField;
+
+    private double xAngle;
+    private double yAngle;
+    private double zAngle;
+    private long ms;
+
+    private double ds;
+    private double d0;
+    private int offset;
+    private int scale;
+
 
     public House3DExample() {
         initUI();
     }
 
     private void initUI() {
-        this.house = new House3D();
-        add(house);
+        //Default values
+        ds = 8;
+        d0 = 10;
+        offset = 390;
+        scale = 270;
+
+        //Initialize fields
+        xAngle = 1;
+        yAngle = 1;
+        zAngle = 1;
+        ms = 10;
+
+        //Frame settings
         setTitle("3D House Rotation");
         setSize(1000, 1000);
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        //Layout
+        getContentPane().setLayout(new BorderLayout());
+        JPanel settingsPanel = new JPanel();
+
+        JLabel label = new JLabel("Check the axis for rotation");
+        settingsPanel.add(label);
+
+        Format format = NumberFormat.getNumberInstance();
+
+        //X axis
+        xCheckbox = new JCheckBox("Rotate X");
+        xAngleTextField = new JFormattedTextField(format);
+        xAngleTextField.setColumns(2);
+        xAngleTextField.setValue(xAngle);
+        settingsPanel.add(xCheckbox);
+        settingsPanel.add(xAngleTextField);
+
+        //Y axis
+        yCheckbox = new JCheckBox("Rotate Y");
+        yAngleTextField = new JFormattedTextField(format);
+        yAngleTextField.setColumns(2);
+        yAngleTextField.setValue(yAngle);
+        settingsPanel.add(yCheckbox);
+        settingsPanel.add(yAngleTextField);
+
+        //Z axis
+        zCheckbox = new JCheckBox("Rotate Z");
+        zAngleTextField = new JFormattedTextField(format);
+        zAngleTextField.setColumns(2);
+        zAngleTextField.setValue(zAngle);
+        settingsPanel.add(zCheckbox);
+        settingsPanel.add(zAngleTextField);
+
+        //Label
+        JLabel msLabel = new JLabel("Refresh rate");
+        msTextField = new JFormattedTextField(format);
+        msTextField.setColumns(3);
+        msTextField.setValue(ms);
+        settingsPanel.add(msLabel);
+        settingsPanel.add(msTextField);
+
+        //Update Button
+        updateButton = new JButton("Update");
+        updateButton.addActionListener(e -> updateValues());
+        settingsPanel.add(updateButton);
+
+        //Add the panel to the top of the window
+        add(settingsPanel, BorderLayout.NORTH);
+
+        //Add the house
+        this.house = new House3D(ds, d0, offset, scale);
+        add(house, BorderLayout.CENTER);
+
+        //Activate
+        setVisible(true);
         repaint();
         run();
     }
 
     public void run() {
-        double angle = Math.PI * 2 / 100;
-        while(true) {
-            house.rotateX(angle);
-            angle += Math.PI * 2 / 100;
+        while (isVisible()) {
+            if (this.xCheckbox.isSelected()) {
+                house.rotateX(xAngle);
+            }
+
+            if (this.yCheckbox.isSelected()) {
+                house.rotateY(yAngle);
+            }
+
+            if (this.zCheckbox.isSelected()) {
+                house.rotateZ(zAngle);
+            }
+
             repaint();
             try {
-                Thread.sleep(100);
+                Thread.sleep(ms);
             } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
+                System.out.println(e);
             }
         }
     }
 
-    public static void main(String args[]) {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                House3DExample ex = new House3DExample();
-                ex.setVisible(true);
-            }
-        });
+    public void updateValues() {
+        try {
+            xAngle = Double.parseDouble(xAngleTextField.getText());
+            yAngle = Double.parseDouble(yAngleTextField.getText());
+            zAngle = Double.parseDouble(zAngleTextField.getText());
+            ms = Long.parseLong(msTextField.getText());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
+
+
+    public static void main(String args[]) {
+        new House3DExample();
+    }
+
 }
